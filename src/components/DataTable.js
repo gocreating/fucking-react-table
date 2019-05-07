b63437d5425b19cb9c7a8484c2582e2703a24963
翁治平 <gocreating@gmail.com>
2019-05-07 23:11:44 +0800

    Add responsive styling

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,7 +1,19 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-const DataTable = styled.table`
+const Wrapper = styled.div`
+  position: relative;
+`
+
+const Scroller = styled.div`
+  overflow-x: auto;
+  overflow-y: visible;
+  width: 100%;
+`
+
+const Table = styled.table`
+  border-collapse: collapse;
+  background-color: #26282b;
 `
 
 const THead = styled.thead`
@@ -10,7 +22,45 @@ const THead = styled.thead`
 const TBody = styled.tbody`
 `
 
+const Tr = styled.tr`
+`
+
+const Th = styled.th`
+  text-align: left;
+  height: 44px;
+  background-color: rgba(255, 255, 255, 0.1);
+  color: #aaaaaa;
+  white-space: nowrap;
+  ${({ cellWidth }) => css`
+    min-width: ${cellWidth}px;
+  `}
+`
+
+const Td = styled.td`
+  text-align: left;
+  height: 62px;
+  background-color: rgba(255, 255, 255, 0.06);
+  white-space: nowrap;
+  color: #ffffff;
+  ${({ cellWidth }) => css`
+    min-width: ${cellWidth}px;
+  `}
+`
+
+const DataTable = ({ children }) => (
+  <Wrapper>
+    <Scroller>
+      <Table>
+        {children}
+      </Table>
+    </Scroller>
+  </Wrapper>
+)
+
 DataTable.THead = THead
 DataTable.TBody = TBody
+DataTable.Tr = Tr
+DataTable.Th = Th
+DataTable.Td = Td
 
 export default DataTable
